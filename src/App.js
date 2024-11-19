3fbba8c2fd07a4cee5618c1b735ba846db973faf
Josher89 <j.hernandezruiz89@gmail.com>
2024-11-18 23:42:03 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [entry, setEntry] = useState([]);
+  const [newEntry, setNewEntry] = useState("");
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/green_diary/')
+      .then(response => {
+        setEntry(response.data);
+      })
+      .catch(error => console.error("There was an error fetching the entry data", error));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newEntry.trim() !== "") {
+      axios.post('http://localhost:8000/api/green_diary/', { text: newEntry })
+        .then(response => {
+          setEntry([response.data, ...entry]);
+          setNewEntry("");
+        })
+        .catch(error => console.error("There was an error fetching the entry data", error));
+    }
+  };
+
+  const handleDelete = (id) => {
+    axios.delete(`http://localhost:8000/api/green_diary/${id}/`)
+      .then(() => {
+        setEntry(entry.filter(entry => entry.id !== id));
+      })
+      .catch(error => console.error("There was an error deleting the Entry data", error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The Green Diary</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newEntry}
+          onChange={(e) => setNewEntry(e.target.value)}
+          placeholder="Write your thoughts down"
+        />
+        <button type="submit">Send Entry</button>
+      </form>
+
+      <ul>
+        {entry.map((entry) => (
+          <li key={entry.id}>
+            {entry.text} - {new Date(entry.timestamp).toLocaleString()}
+            <button onClick={() => handleDelete(entry.id)}>Delete entry</button>
+          </li>
+        ))}
+      </ul>
+
+      <h1>{entry.text}</h1>
+
     </div>
   );
 }
